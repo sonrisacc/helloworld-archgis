657a6644e1baeb571ffb5040d158f9c04328558b
sonrisa722611 <sonrisa722611@gmail.com>
2020-05-09 16:34:04 -0700

    Fine..

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,40 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { EsriMapComponent } from "./esri-map/esri-map.component";
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
+describe("AppComponent", () => {
+  let fixture;
+  let app: any;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, EsriMapComponent]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+  });
+
+  it("should create the app", async(() => {
+    expect(app).toBeTruthy();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  it("should set zoom level", async(() => {
+    expect(app.mapZoomLevel).toEqual(jasmine.any(Number));
+  }));
 
-  it(`should have as title 'esri-map-v1'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('esri-map-v1');
-  });
+  it("should set basemap type", async(() => {
+    expect(app.basemapType).toEqual(jasmine.any(String));
+    expect(app.basemapType).toEqual("satellite");
+  }));
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('esri-map-v1 app is running!');
-  });
+  it("should set map center location", async(() => {
+    expect(app.mapCenter).toEqual(jasmine.any(Array));
+    expect(app.mapCenter.length).toEqual(2);
+  }));
+
+  it("zoom has a default value", async(() => {
+    expect(app.mapZoomLevel).toBeGreaterThanOrEqual(0);
+    expect(app.mapZoomLevel).toBeLessThanOrEqual(24);
+  }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { EsriMapComponent } from "./esri-map/esri-map.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EsriMapComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
